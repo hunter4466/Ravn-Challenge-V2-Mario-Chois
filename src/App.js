e4826a6a4cb9f96b47ac92bd0221c0dfa6683550
Mario Chois <mario.chois.navarro@gmail.com>
2021-12-10 19:54:18 -0500

    GraphQL: Setup GraphQL to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query getAllFlms {
+    allFilms{
+      films {title, episodeID, openingCrawl}
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className="App">
-      App
+      {data.allFilms.films.map(({ title, episodeID, openingCrawl }) => (
+        <div key={episodeID}>
+          <p>
+            {title}
+            :
+            {openingCrawl}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
