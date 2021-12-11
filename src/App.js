dcaf0355aa3d50fa9592f77e59658079ac6f27e1
Mario Chois <mario.chois.navarro@gmail.com>
2021-12-10 22:11:01 -0500

    Router: Set up router ofr navigation bar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
 import { useQuery, gql } from '@apollo/client';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
-const EXCHANGE_RATES = gql`
+function App() {
+  const EXCHANGE_RATES = gql`
   query getAllFlms {
-    allFilms{
-      films {title, episodeID, openingCrawl}
+    allPeople (first: 5) {
+      people {id, name, species {name}, homeworld {
+        name
+      }}
     }
   }
 `;
-
-function App() {
   const { loading, error, data } = useQuery(EXCHANGE_RATES);
-
+  console.log(data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-
-  return (
-    <div className="App">
-      {data.allFilms.films.map(({ title, episodeID, openingCrawl }) => (
-        <div key={episodeID}>
-          <p>
-            {title}
-            :
-            {openingCrawl}
-          </p>
+  return (data.allPeople.people ? (
+    <div>
+      <div className="header_bar">
+        <h1>Ravn Star Wars Registry</h1>
+      </div>
+      <Router>
+        <div className="navigator_bar">
+          {data.allPeople.people.map(({
+            id, name, species, homeworld,
+          }) => (
+            <NavLink key={id} activeClassName="selected_nav_item" className="nav_item" to={`/${id}`}>
+              {name}
+              {species ? species.name : 'Human'}
+              {homeworld.name}
+            </NavLink>
+          ))}
         </div>
-      ))}
+        <Switch>
+          {data.allPeople.people.map(({
+            id, name, species, homeworld,
+          }) => (
+            <Route key={id} exact path={`/${id}`}>
+              <div>
+                {name}
+                {species ? species.name : 'Human'}
+                {homeworld.name}
+              </div>
+            </Route>
+          ))}
+        </Switch>
+      </Router>
     </div>
+  ) : false
   );
 }
 
