594d8876fb15e8ec4ad16b7bd3b65406d32587a1
Mario Chois <mario.chois.navarro@gmail.com>
2021-12-11 21:06:15 -0500

    View: Add functionality to display data on view

--- src/components/modules/NavigationView.js
+++ src/components/modules/NavigationView.js
@@ -8,30 +8,30 @@ const NavigationView = ({
 }) => (
   <Route exact path={`/${nodeid}`}>
     <div className="view_main_containter">
-      <div className="view_top_container">
-        <h1>General Information</h1>
+      <div className="view_container">
+        <h1 className="view_title">General Information</h1>
         <div className="view_element">
-          <h2 className="view_element_">Eye Color</h2>
-          <h2>{eyeColor}</h2>
+          <h2 className="view_element_indicator">Eye Color</h2>
+          <h2 className="view_element_data">{eyeColor}</h2>
         </div>
         <div>
-          <h2>Hair Color</h2>
-          <h2>{hairColor}</h2>
+          <h2 className="view_element_indicator">Hair Color</h2>
+          <h2 className="view_element_data">{hairColor}</h2>
         </div>
         <div>
-          <h2>Skin Color</h2>
-          <h2>{skinColor}</h2>
+          <h2 className="view_element_indicator">Skin Color</h2>
+          <h2 className="view_element_data">{skinColor}</h2>
         </div>
         <div>
-          <h2>Birth Year</h2>
-          <h2>{birthYear}</h2>
+          <h2 className="view_element_indicator">Birth Year</h2>
+          <h2 className="view_element_data">{birthYear}</h2>
         </div>
       </div>
-      <div>
-        <h1>Vehicles</h1>
+      <div className="view_container">
+        <h1 className="view_title">Vehicles</h1>
         {vehicles.map(({ name, id }) => (
           <div key={id}>
-            <h2>{name}</h2>
+            <h2 className="view_element_indicator">{name}</h2>
           </div>
         ))}
       </div>
