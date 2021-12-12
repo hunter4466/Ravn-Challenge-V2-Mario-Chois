d58f6752578dd377ef4351b7b349b2dfc2901e6d
Mario Chois <mario.chois.navarro@gmail.com>
2021-12-11 21:45:35 -0500

    Styles: Add styles for view nad its elements

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ravn-Challenge-V2-Mario-Chois
 Ravn Challenge Application, used for displaying information with StarWars Theme.
+ node v 14.16.0
--- src/components/modules/NavigationView.js
+++ src/components/modules/NavigationView.js
@@ -5,39 +5,56 @@ import { Route } from 'react-router-dom';
 
 const NavigationView = ({
   nodeid, eyeColor, hairColor, skinColor, birthYear, vehicles,
-}) => (
-  <Route exact path={`/${nodeid}`}>
-    <div className="view_main_containter">
-      <div className="view_container">
-        <h1 className="view_title">General Information</h1>
-        <div className="view_element">
-          <h2 className="view_element_indicator">Eye Color</h2>
-          <h2 className="view_element_data">{eyeColor}</h2>
-        </div>
-        <div>
-          <h2 className="view_element_indicator">Hair Color</h2>
-          <h2 className="view_element_data">{hairColor}</h2>
-        </div>
-        <div>
-          <h2 className="view_element_indicator">Skin Color</h2>
-          <h2 className="view_element_data">{skinColor}</h2>
-        </div>
-        <div>
-          <h2 className="view_element_indicator">Birth Year</h2>
-          <h2 className="view_element_data">{birthYear}</h2>
-        </div>
-      </div>
-      <div className="view_container">
-        <h1 className="view_title">Vehicles</h1>
-        {vehicles.map(({ name, id }) => (
-          <div key={id}>
-            <h2 className="view_element_indicator">{name}</h2>
+}) => {
+  const parser = (str) => {
+    if (str === 'n/a') {
+      return 'Unknown';
+    }
+    const capt = str.charAt(0).toUpperCase() + str.slice(1);
+    return capt;
+  };
+  return (
+    <Route exact path={`/${nodeid}`}>
+      <div className="view_main_containter">
+        <div className="view_container">
+          <div className="view_title_wrapper">
+            <h1 className="view_title">General Information</h1>
           </div>
-        ))}
+          <div className="view_element">
+            <h2 className="view_element_indicator">Eye Color</h2>
+            <h2 className="view_element_data">{parser(eyeColor)}</h2>
+          </div>
+          <div className="view_element">
+            <h2 className="view_element_indicator">Hair Color</h2>
+            <h2 className="view_element_data">{parser(hairColor)}</h2>
+          </div>
+          <div className="view_element">
+            <h2 className="view_element_indicator">Skin Color</h2>
+            <h2 className="view_element_data">{parser(skinColor)}</h2>
+          </div>
+          <div className="view_element">
+            <h2 className="view_element_indicator">Birth Year</h2>
+            <h2 className="view_element_data">{parser(birthYear)}</h2>
+          </div>
+        </div>
+        <div className="view_container">
+          <div className="view_title_wrapper">
+            <h1 className="view_title">Vehicles</h1>
+          </div>
+          {vehicles.length > 0 ? (vehicles.map(({ name, id }) => (
+            <div key={id} className="view_element">
+              <h2 className="view_element_indicator">{name}</h2>
+            </div>
+          ))) : (
+            <div className="view_element">
+              <h2 className="view_element_no_vehicles">No vehicles found</h2>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  </Route>
-);
+    </Route>
+  );
+};
 
 NavigationView.propTypes = {
   nodeid: PropTypes.string.isRequired,
